4b0c961e7c0bdee6bbf7334dbe9f4458cfef8a22
Gooseglasses <127079609+ccccler@users.noreply.github.com>
2024-06-06 16:51:49 +0800

    Update repair.py

--- repair.py
+++ repair.py
@@ -178,4 +178,111 @@ class Repair:
 
         print("报告修改完毕，请在[修改报告档案.md]中查看")
 
+    def extre(self,raw_data,input):
+
+    client = self.config_client
+    system_prompt = '''你是一个数据提取机器人。你的作用是从一段文本当中提取出可能存在的数据的多个共同属性,以方便将这段文本转化成一份结构化的数据。
+                你所要输出的是一个个若干个属性名称、各个属性所适合的数据类型（字符、数值、类别）以及关于该属性的描述，并以dict的格式反馈给我。
+                dict的格式是：  {"维度n": {
+                "name": "",
+                "type": "",
+                "discription":""
+                    }}
+                请你只反馈给我属性的dict格式信息，不要返回任何其他不相关的内容。注意，你只需要返回形式为dict的字符串，而无需返回任何其他的内容。
+                不要放在类似```的BACKTICKS里。'''
+
+    user_prompt = '''根据最新的销售数据显示，比亚迪以133,317辆的销量稳居榜首，占据了整体市场的10.29%份额。紧随其后的是长安汽车，
+    其销量达到90,067辆，占据了6.95%的市场份额。而第三名则被上汽大众所占据，其销量为78,000辆，占据了整体市场的6.02%份额。
+    一汽-大众以70,004辆的销量位列第四，占据了5.41%的市场份额。吉利汽车以67,479辆的销量排名第五，占据了5.21%的市场份额。'''
+
+    ai_prompt = '''{
+      "维度1": {
+        "name": "排名",
+        "type": "int",
+        "discription":"该属性描述了数据中各个汽车品牌的销售排名"
+      },
+      "维度2": {
+        "name": "品牌名称",
+        "type": "str",
+        "discription":"该属性描述了数据中各个汽车品牌的名称"
+      },
+      "维度3": {
+        "name": "销量",
+        "type": "int",
+        "discription":"该属性描述了数据中各个汽车品牌的销量"
+      },
+      "维度4": {
+        "name": "占销量份额",
+        "type": "int",
+        "discription":"该属性描述了数据中各个汽车品牌销量所占的销售份额"
+      },
+    }'''
+
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+            {"role": "assistant", "content": ai_prompt},
+            {"role": "user", "content": f"请你根据{input}的要求，按照案例提取该数据的维度，数据是{raw_data}"},
+        ],
+        temperature=0
+    )
+
+    reitems = completion.choices[0].message.content
+
+    system_prompt = '''你是一个数据提取机器人，擅长将非结构化数据转化为dataframe格式的结构化数据。我将给你提供一段非结构化数据data，
+            以及该数据中存在的各个数据属性item。
+            请你根据item，将data当中对应的数据一个不落的归纳为一份完整的结构化的dataframe格式数据。
+            请注意，你只被允许输出一份完整的dataframe格式的数据。我将给你提供一个例子。
+            请你严格按照这个例子的形式，返回一种不带BACKTICKS```符号的str格式的数据文本，不允许输出任何以与dataframe格式数据无关的内容。'''
+    user_prompt = '''data："根据最新的销售数据显示，比亚迪以133,317辆的销量稳居榜首，占据了整体市场的10.29%份额。
+            紧随其后的是长安汽车，其销量达到90,067辆，占据了6.95%的市场份额。而第三名则被上汽大众所占据，其销量为78,000辆，
+            占据了整体市场的6.02%份额。一汽-大众以70,004辆的销量位列第四，占据了5.41%的市场份额。吉利汽车以67,479辆的销量排名第五，
+            占据了5.21%的市场份额。"
+        item:"{
+          "attribute1": {
+            "name": "排名",
+            "type": "int"
+            "discription":"该属性描述了数据中各个汽车品牌的销售排名"
+          },
+          "attribute2": {
+            "name": "厂商",
+            "type": "str"
+            "discription":"该属性描述了数据中各个汽车品牌的名称"
+          },
+          "attribute3": {
+            "name": "销量",
+            "type": "int"
+            "discription":"该属性描述了数据中各个汽车品牌的销量"
+          }"
+          '''
+    ai_prompt = '''排名	厂商	销量	
+        1	比亚迪	133317	
+        2	长安汽车	90067	
+        3	上汽大众	78000	
+        4	一汽-大众	70004	
+        5	吉利汽车	67479	'''
+
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+            {"role": "assistant", "content": ai_prompt},
+            {"role": "user", "content": "data:" + raw_data + "items:" + reitems},
+        ],
+        temperature=0
+    )
+
+    format_data = completion.choices[0].message.content
+
+    md_data = f"""修改要求是：\n{input}\n修改完成的items是:\n{reitems}\n新数据是\n{format_data}"""
+
+    with open('./temp/数据修改档案.md', 'w', encoding='utf-8') as file:
+        file.write(md_news)
+
+    print("数据修改完毕，请在[数据修改档案.md]中查看")
+
+
 
